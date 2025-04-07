6d60a7c9eea32d02166d5051d6b9b85341dc1440
Alex Foster <ajf63@students.uwf.edu>
2025-04-06 23:48:45 -0500

    Remove redundant Patient class definition from patient.cpp

--- patient.cpp
+++ patient.cpp
@@ -1,32 +1,5 @@
 #include "patient.hpp"
-
-class Patient {
-    private:
-        std::string name; // Add private member for name
-        int age;
-        std::string gender;
-    
-    public:
-        // Default constructor
-        Patient() : name(""), age(0), gender("") {}
-    
-        // Parameterized constructor
-        Patient(const std::string& name, int age, const std::string& gender)
-            : name(name), age(age), gender(gender) {}
-    
-        // Getter for name
-        std::string getName() const {
-            return name; // Implement getter for name
-        }
-    
-        // Method to print patient information
-        void printInfo() const {
-            std::cout << "Patient name: " << name << "\n"
-                      << "Patient age: " << age << "\n"
-                      << "Patient gender: " << gender << std::endl;
-        }
-    };
-
+#include <iostream>
 
 // Default constructor
 Patient::Patient() : name(""), age(0), gender("") {}
